4f1b282e61ed9274ce3e48a42fd983c8cab7e69d
Juan Castillo <Juanpablodlcc@gmail.com>
2020-03-23 20:52:13 -0600

    fix styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import crg from 'country-reverse-geocoding'
 import Card from './Card'
 
-const Main = ({country, confirmed, deaths, recovered, date}) => {
+const Main = ({ country, confirmed, deaths, recovered, date }) => {
   return (
     <div className="card mb-3 main">
       <div className="row no-gutters">
@@ -28,7 +28,6 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      location: {},
       current: {},
       locations: [],
       confirmed: 0,
@@ -38,13 +37,16 @@ class App extends Component {
       sort: "deaths"
     }
   }
+  setup() {
+
+  }
 
   async componentWillMount() {
     navigator.geolocation.getCurrentPosition(({ coords }) => {
       const { latitude, longitude } = coords;
       const { name: country } = crg.country_reverse_geocoding().get_country(latitude, longitude);
       this.setState({
-        location: {
+        current: {
           latitude,
           longitude,
           country
@@ -54,6 +56,10 @@ class App extends Component {
     const res = await fetch("https://covid-19api.com/api/countries-latest", { mode: 'cors' })
     const countries = await res.json()
     for (const country of countries) {
+      if (this.state.current.country) {
+        const current = this.state.locations.filter(location => location.country === this.state.current.country)
+        this.setState({ current: { ...current.shift(), ...this.state.current } })
+      }
       this.setState({
         locations: [...this.state.locations, country].sort((a, b) => b[this.state.sort] - a[this.state.sort]),
         confirmed: this.state.confirmed + country.confirmed,
@@ -61,61 +67,81 @@ class App extends Component {
         recovered: this.state.recovered + country.recovered,
       })
     }
-    const current = (this.state.locations.filter(location => location.country === this.state.location.country)).shift()
-    this.setState({ current })
+
   }
 
   queryOnChange({ target }) {
     this.setState({ query: target.value })
   }
-
+  checkInput(location) {
+    return this.state.query ?
+      location.country.toUpperCase().indexOf(this.state.query.toUpperCase()) > -1 :
+      Boolean
+  }
 
   render() {
     return (
       <>
-        <header className="App-header">
-          <br />
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Covid-19 <em>  Tracker</em></p>
-          <br />
-        </header>
-        <body className="text-center">
-          <div className="App-header">
-            <div className="row">
-              {
-                this.state.current ?
-                  <Main 
+        <div className="App-header">
+          <header className="App-header">
+
+            <br />
+            <img src={logo} className="App-logo" alt="logo" />
+            <br />
+            <h3>Covid-19 <em>  Tracker</em></h3>
+            <h4>#StayAtHome</h4>
+            <br />
+          </header>
+          <body className="text-center">
+            <div className="container-xl">
+              <div className="row list principal">
+                {
+                  this.state.current.country ?
+                    <Main
+                      className="col col-12 "
                       country={this.state.current.country}
-                        confirmed={this.state.current.confirmed}
-                        deaths={this.state.current.deaths}
-                        recovered={this.state.current.recovered}
-                        date={this.state.current.date}
-                  />
-                  : null
-              }
+                      confirmed={this.state.current.confirmed}
+                      deaths={this.state.current.deaths}
+                      recovered={this.state.current.recovered}
+                      date={this.state.current.date}
+                    /> :
+                    <h3>Search your country</h3>}
+              </div>
+              <input
+                type="text" placeholder="Search a country..."
+                onChange={this.queryOnChange.bind(this)}>
+              </input>
+              <br />
+
+              <div className="container" >
+                <div className="row list">
+                  {
+                    this.state.locations.length ?
+                      this.state.locations
+                        .filter(location => this.checkInput(location))
+                        .map((location, key) => {
+                          return location.country !== this.state.current.country ?
+                            <Card key={key}
+                              country={location.country}
+                              confirmed={location.confirmed}
+                              deaths={location.deaths}
+                              recovered={location.recovered}
+                              date={location.date}
+                            /> : null
+
+                        }) :
+                      <div className="d-flex justify-content-center App-header" style={{ marginLeft: "40%" }}>
+                        <div className="spinner-border" role="status">
+                          <span className="sr-only">Loading...</span>
+                        </div>
+                      </div>
+
+                  }
+                </div>
+              </div>
             </div>
-            <input type="text" placeholder="Search a country..." onChange={this.queryOnChange.bind(this)}></input>
-            <div className="row list">
-              {
-                this.state.locations
-                  .filter(location => this.state.query ?
-                    location.country.toUpperCase().indexOf(this.state.query.toUpperCase()) > -1 :
-                    Boolean)
-                  .map((location, key) => {
-                    return location.country !== this.state.location.country ?
-                      <Card key={key}
-                        country={location.country}
-                        confirmed={location.confirmed}
-                        deaths={location.deaths}
-                        recovered={location.recovered}
-                        date={location.date}
-                      />
-                      : null
-                  })
-              }
-            </div>
-          </div>
-        </body>
+          </body>
+        </div>
       </>
     );
   }
