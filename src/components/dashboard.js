b24b3bda8317d507bd32086caa148efd021dc0b6
Juan Castillo <Juanpablodlcc@gmail.com>
2020-04-20 10:33:22 -0600

    testing

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -99,7 +99,7 @@ class Dashboard extends Component {
                                                 <Modal.Header closeButton>
                                                     <Modal.Title>Modal heading</Modal.Title>
                                                 </Modal.Header>
-                                                <Modal.Body>Woohoo, im still working on whit D:!</Modal.Body>
+                                                <Modal.Body>Woohoo, im still working on this D:!</Modal.Body>
                                                 <Modal.Footer>
                                                     <Button variant="secondary" onClick={this.handleClose}>
                                                         Close
